3144ce30a8094d4392fda5516d0e7f56611c21ca
Putra Irawan <putrairawan993@gmail.com>
2020-12-16 20:42:00 +0700

    [Add]Landing-page

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ThemeProvider, makeStyles, CssBaseline, Container, Grid } from '@material-ui/core';
+import { Card } from './components/card/Card';
+import Header from './components/header/Header';
+import theme from './utils/theme';
+import Footer from './components/footer/Footer';
+import LandingPage from './pages/landingPage/LandingPage';
+import AboutUs from './pages/aboutUs/AboutUs';
+
+class App extends Component {
+	render() {
+		return (
+			<div style={{ overflowX: 'hidden' }}>
+				<Header />
+				{/* <Card /> */}
+				<LandingPage />
+				{/* <AboutUs /> */}
+				<Footer />
+				<CssBaseline />
+			</div>
+		);
+	}
+}
+
+export default App;
