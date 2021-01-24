373610593cf7e6946647d9d47b1c003679ac395a
Yogi Prsetya <yogiprsetya@gmail.com>
2021-01-24 21:18:13 +0700

    setup add partner modal

--- src/lib/components/Popup/AddNewPartner.jsx
+++ src/lib/components/Popup/AddNewPartner.jsx
@@ -7,7 +7,9 @@ import { create, read } from 'utils/api';
 
 const AddNewPartner = ({ dispatch }) => {
   const [preview, setPreview] = useState(null);
-  const [companyName, setCompanyName] = useState([]);
+  const [companyName, setCompanyName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [country, setCountry] = useState('');
   const [city, setCity] = useState('');
   const [categoryId, setCategoryId] = useState('');
@@ -15,13 +17,23 @@ const AddNewPartner = ({ dispatch }) => {
 
   const onChangeImage = e => {
     e.preventDefault();
-    setPreview(URL.createObjectURL(e.target.files[0]));
-    console.log(e.target.files[0])
+    setPreview(e.target.files[0]);
   }
 
   const submit = e => {
     e.preventDefault();
-    create('admin/services');
+    const formData = new FormData();
+
+    formData.append('file', preview);
+    formData.append('company_name', companyName);
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('country', 'Vietnam');
+    formData.append('city', 'Hanoi');
+    formData.append('category_id', categoryId);
+
+    create('admin/partners', formData)
+    .then(res => console.log(res))
   };
 
   useEffect(() => {
@@ -45,29 +57,30 @@ const AddNewPartner = ({ dispatch }) => {
       </div>
 
       <div className="flex flex-col">
-        <div className="mx-3 py-5 px-2 mb-4 flex items-center">
-          <label className="w-24 h-24 text-center flex flex-col cursor-pointer rounded-full justify-center border-2 border-dashed">
-            { preview ? <img src={ preview } /> : <Icon name="image" size={30} className="mx-auto" color="#6493b9" /> }
-
-            { !preview && <small className="text-xxs text-blue">
-              Add file or drag<br/>& drop here
-            </small> }
-
-            <input
-              type="file"
-              accept=".jpg,.jpeg,.png,.gif"
-              className="hidden"
-              onChange={onChangeImage}
-            />
-          </label>
-
-          <div className="ml-5">
-            <b className="leading-3 block">Profile Image</b>
-            <small>Supports: JPG, JPEG, PNG, GIF</small>
-          </div>
-        </div>
-
         <form onSubmit={ submit }>
+          <div className="mx-3 py-5 px-2 flex items-center">
+            <label className="w-24 h-24 text-center flex flex-col cursor-pointer rounded-full justify-center border-2 border-dashed">
+              { preview ? <img src={ URL.createObjectURL(preview) } /> : <Icon name="image" size={30} className="mx-auto" color="#6493b9" /> }
+
+              { !preview && <small className="text-xxs text-blue">
+                Add file or drag<br/>& drop here
+              </small> }
+
+              <input
+                type="file"
+                required
+                accept=".jpg,.jpeg,.png,.gif"
+                className="hidden"
+                onChange={onChangeImage}
+              />
+            </label>
+
+            <div className="ml-5">
+              <b className="leading-3 block">Profile Image</b>
+              <small>Supports: JPG, JPEG, PNG, GIF</small>
+            </div>
+          </div>
+
           <div className="mx-3 flex flex-wrap">
             <label className="w-full px-2 flex flex-col mb-5">
               <span className="mr-5 mb-2 text-sm font-bold">Company Name</span>
@@ -75,6 +88,7 @@ const AddNewPartner = ({ dispatch }) => {
               <input
                 required
                 className="border py-1 px-2"
+                onChange={ e => setCompanyName(e.target.value) }
               />
             </label>
 
@@ -83,7 +97,20 @@ const AddNewPartner = ({ dispatch }) => {
 
               <input
                 required
+                type="email"
                 className="border py-1 px-2"
+                onChange={ e => setEmail(e.target.value) }
+              />
+            </label>
+
+            <label className="w-full px-2 flex flex-col mb-5">
+              <span className="mr-5 mb-2 text-sm font-bold">Password</span>
+
+              <input
+                required
+                type="password"
+                className="border py-1 px-2"
+                onChange={ e => setPassword(e.target.value) }
               />
             </label>
 
@@ -107,7 +134,10 @@ const AddNewPartner = ({ dispatch }) => {
             <label className="w-full px-2 flex flex-col mb-5">
               <span className="mr-5 mb-2 text-sm font-bold">Service Category</span>
 
-              <select className="border p-2 text-xs text-gray-500">
+              <select
+                className="border p-2 text-xs text-gray-500"
+                onChange={ e => setCategoryId(e.target.value) }
+              >
                 <option>Select country</option>
                 { listCategory.map(el => (
                   <option key={ el.id } value={ el.id }>{ el.name }</option>
--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -4,6 +4,7 @@ const setAuthToken = token => {
   if (token) {
     axios.defaults.baseURL = `${process.env.REACT_APP_API_HOST}`;
     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    axios.defaults.headers.common.Accept = 'application/json';
   } else {
     delete axios.defaults.headers.common.Authorization;
   }
